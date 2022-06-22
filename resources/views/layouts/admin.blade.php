23e44cfd21d30155f412454a73e526d07bfcc575
nehabht <ne.bhatti2@icloud.com>
2022-06-22 23:14:10 +0200

    add logout button

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -50,7 +50,7 @@
                             @endif
                         @else
 
-                            <!-- <li class="nav-item">
+                            <li class="nav-item">
                                 <a class="nav-link" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
@@ -60,7 +60,7 @@
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
-                            </li> -->
+                            </li>
 
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
