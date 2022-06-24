45667a85db06ce6a2d06140059acfabfa3e73975
nehabht <ne.bhatti2@icloud.com>
2022-06-24 23:09:39 +0200

    add fix front js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,6 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import App from './views/App'
 
 /**
  * The following block of code may be used to automatically register your
@@ -31,5 +30,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
-    render: h => h(App) 
+
 });
